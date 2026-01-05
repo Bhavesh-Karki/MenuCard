ac7214bd28290b9efdd55da011e8275b237d6100
Bhavesh-Karki <codebybhavesh@gmail.com>
2026-01-05 16:47:10 +0530

    add css in all buttons

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -6,7 +6,9 @@ import Detail from './components/Detail';
 function App() {
   return (
     <div className="App">
-      <h1>Menu Card</h1>
+    <h1 style={{ color: "Black", fontSize: "32px", textAlign: "center", fontWeight: "bold", marginBottom: "20px", fontFamily: "Times New Roman"}}>
+  MENU CARD
+</h1>
       <hr />
       <ArrayMethods />
       <Detail />
--- Frontend/frontend/src/components/Detail.js
+++ Frontend/frontend/src/components/Detail.js
@@ -8,7 +8,7 @@ function Detail() {
     <div className="detail-container">
       {/* First Section */}
       <div className="card-section">
-        <Card className="menu-card">
+        <Card className="menu-card" id="1">
           <Card.Img variant="top" src={pancakeImage} className="card-image" />
           <Card.Body>
             <Card.Title>Delicious Pancake</Card.Title>
@@ -19,7 +19,7 @@ function Detail() {
           </Card.Body>
         </Card>
 
-        <Card className="menu-card">
+        <Card className="menu-card" id="2">
         <Card.Img
         variant="top"
         src="https://raw.githubusercontent.com/john-smilga/javascript-basic-projects/master/08-menu/final/images/item-2.jpeg"
@@ -34,7 +34,7 @@ function Detail() {
           </Card.Body>
         </Card>
 
-        <Card className="menu-card">
+        <Card className="menu-card" id="3">
           <Card.Img variant="top" src="https://raw.githubusercontent.com/john-smilga/javascript-basic-projects/refs/heads/master/08-menu/final/images/item-10.jpeg" 
           className="card-image" 
           alt='Chicken'/>
@@ -47,7 +47,7 @@ function Detail() {
           </Card.Body>
         </Card>
 
-        <Card className="menu-card">
+        <Card className="menu-card" id="4">
           <Card.Img variant="top" src="https://imgs.search.brave.com/ectHTxT9ESItQhMnntN9qeuMcPBcPoxenBaUNt4wVOo/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9oeWZ1/bmZvb2RzLmNvbS93/cC1jb250ZW50L3Vw/bG9hZHMvMjAyNC8w/NS9TdXBlci1Dcmlz/cHktQ29hdGVkLUZy/ZW5jaC1Gcmllc19I/aWdoLVJlcy0xLnBu/Zw" className="card-image" alt='french fries' />
           <Card.Body>
             <Card.Title>French Fries</Card.Title>
@@ -60,8 +60,8 @@ function Detail() {
       </div>
 
       {/* Second Section */}
-      <div className="card-section">
-        <Card className="menu-card">
+      <div className="card-section" >
+        <Card className="menu-card" id="5">
           <Card.Img variant="top" src="https://imgs.search.brave.com/q2oVlhIfOcly1-RhZ7QBMDnvbKmcTjU-kHIPqm6ovn0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/ZG9taW5vcy5jby5p/bi90aGVtZTIvZnJv/bnQvaW1hZ2VzL21l/bnUtaW1hZ2VzL215/LXBpenphbWFuaWEu/d2VicA" className="card-image" alt='pizza' />
           <Card.Body>
             <Card.Title>Pizza</Card.Title>
@@ -72,7 +72,7 @@ function Detail() {
           </Card.Body>
         </Card>
 
-        <Card className="menu-card">
+        <Card className="menu-card" id="6">
           <Card.Img variant="top" src="https://imgs.search.brave.com/j9f85j5w8T2-9_VmY536adsqAAFFLnrAPRbTGxDt1Q0/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wMDEv/ODI2Lzk2MS9zbWFs/bC9zYXV0ZWVkLW1h/Y2Fyb25pLXBhc3Rh/LWluLXRoZS1wYW4t/ZnJlZS1waG90by5q/cGc" className="card-image" alt='pasta' />
           <Card.Body>
             <Card.Title>Pasta</Card.Title>
@@ -83,7 +83,7 @@ function Detail() {
           </Card.Body>
         </Card>
 
-        <Card className="menu-card">
+        <Card className="menu-card"  id="7">
           <Card.Img variant="top" src="https://imgs.search.brave.com/avXk_mNo2RY05NRu185UZhtzgaB8fpJ8hH51CyokBTo/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxNS8x/Mi8xNi8wOS80MC9z/YWxhZC1wbGF0ZS0x/MDk1NjQ4XzY0MC5q/cGc" className="card-image" alt='salad' />
           <Card.Body>
             <Card.Title>Salad</Card.Title>
@@ -94,7 +94,7 @@ function Detail() {
           </Card.Body>
         </Card>
 
-        <Card className="menu-card">
+        <Card className="menu-card" id="8">
           <Card.Img variant="top" src="https://imgs.search.brave.com/IAQk1hEdb7aVC7i8Z0dupMCGmZySIkm8EWbyzDfAlzQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA1LzU4LzEzLzAz/LzM2MF9GXzU1ODEz/MDMyM19HNTN6N0Fy/a2FiSlhydlNBUVlp/THhodXFOWE4weWVp/di5qcGc" className="card-image" alt='ice cream' />
           <Card.Body>
             <Card.Title>Ice Cream</Card.Title>
